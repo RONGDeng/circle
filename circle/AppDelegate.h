c59b7a924f81b68eb9a4d87e12f11c3b14ecb6ee
tripoke <tripoke@tripoketekimac-mini.local>
2016-02-17 17:22:14 -0800

    Initial Commit

---
+++ circle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  circle
+//
+//  Created by tripoke on 16/2/17.
+//  Copyright © 2016年 tripoke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ circle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  circle
+//
+//  Created by tripoke on 16/2/17.
+//  Copyright © 2016年 tripoke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
